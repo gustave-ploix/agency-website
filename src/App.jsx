30e19215e79b2013e8f865152e6086cc1c717733
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-21 21:44:37 +0200

    permit a toggle between close and open the menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 
@@ -6,11 +8,13 @@ import './style.scss'
 
 export default function App() {
 
+    const [deployMenu, setdeployMenu] = useState(false);
+
 
     return (
         <>
-            <Navbar />
-            <Header />
+            <Navbar toggle={setdeployMenu} state={deployMenu}  />
+            <Header menuState={deployMenu} />
         </>
     )
 }
