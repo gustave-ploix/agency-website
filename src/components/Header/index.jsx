8d622e80e3f3b35aa60a8af375fc2d6560faab13
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-21 21:21:40 +0200

    stylized the header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+import './style.scss';
+
+function Header() {
+
+    
+    return ( 
+        <section>
+            <ul>
+                <li>Intro</li>
+                <li>About</li>
+                <li>Featured</li>
+            </ul>
+
+            {/* <h1>Art <span>Objects</span> </h1> */}
+            <div>
+                <h1>ART</h1>
+                <h1>OBJECTS</h1>
+            </div>
+
+        </section>
+     )
+}
+
+export default Header;
