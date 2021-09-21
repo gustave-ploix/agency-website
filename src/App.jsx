4ee8c6f0c7a8b5a24d0eb03ab3774d307958e857
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-21 20:07:36 +0200

    did a reset of the css properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Navbar from './components/Navbar';
+
+import './style.scss'
+
+
 export default function App() {
+
+
     return (
-        <p>bonjour</p>
+        <>
+            <Navbar />
+        </>
     )
 }
