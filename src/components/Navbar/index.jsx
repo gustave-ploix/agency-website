4908cc753e8aa647ddfef0cdfff33136c8a65190
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-21 20:07:58 +0200

    created the navbar with three sections

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import './style.scss'
+
+function Navbar() {
+    return (
+
+        <nav>
+            <div>Menu</div>
+            <div>Flirty Flowers</div>
+            <div>Cart</div>
+        </nav>
+
+    );
+}
+
+export default Navbar;
