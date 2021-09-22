41399af9756e9004aa62b32d0bdbb9d806a4efbb
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-22 20:14:45 +0200

    added title and subtitle field to the gallery images

--- src/services/photosGallery.js
+++ src/services/photosGallery.js
@@ -1,19 +1,20 @@
 const images = [{
-    "src": "https://images.unsplash.com/photo-1482148454461-aaedae4b30bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
+    "src": "https://images.unsplash.com/photo-1482148454461-aaedae4b30bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+    "title":"Lorem Ipsum",
+    "subtitle": "Lorem Ipsum"
 },{
-    "src": "https://images.unsplash.com/photo-1482146426705-433fc4949dbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80"
+    "src": "https://images.unsplash.com/photo-1482146426705-433fc4949dbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
+    "title":"Lorem Ipsum",
+    "subtitle": "Lorem Ipsum"
 }, {
-    "src": "https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1674&q=80"
+    "src": "https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1674&q=80",
+    "title":"Lorem Ipsum",
+    "subtitle": "Lorem Ipsum"
 }, {
-    "src" : "https://images.unsplash.com/photo-1530518854704-23de978d2915?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2586&q=80"
+    "src" : "https://images.unsplash.com/photo-1530518854704-23de978d2915?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2586&q=80",
+    "title":"Lorem Ipsum",
+    "subtitle": "Lorem Ipsum"
 }
-]
-
-// const images = [
-//     "https://images.unsplash.com/photo-1482148454461-aaedae4b30bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
-//     "https://images.unsplash.com/photo-1482146426705-433fc4949dbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80",
-//     "https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1674&q=80",
-//     "https://images.unsplash.com/photo-1530518854704-23de978d2915?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2586&q=80"
-// ]
+];
 
 export default images;
