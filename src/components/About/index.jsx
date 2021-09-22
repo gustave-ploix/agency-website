726bef938359d8dfc5653ec03b158b3995a71438
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-22 14:41:25 +0200

    created and stylized the about container

---
+++ src/components/About/index.jsx
@@ -0,0 +1,17 @@
+import './style.scss';
+
+function About() {
+  return (
+    <section id="aboutSection">
+      <h5>ABOUT</h5>
+      <p>
+        Flirty Flowers is a blog about flowers and the floral designers who make
+        them into art. Creativity and the art of ‘making’ require dialogue. The
+        full purpose of the Flirty Flowers blog is to encourage and inspire. We
+        value art, we value insight, and we value opinion.
+      </p>
+    </section>
+  );
+}
+
+export default About;
