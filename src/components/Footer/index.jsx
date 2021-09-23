a6d8596f40e5dbdde0ab16267fb7b35807e84272
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-23 13:07:12 +0200

    created and stylized the footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import './style.scss'
+
+
+function Footer() {
+    return (  
+        <section id="footer">
+            <h1>
+                made in
+            </h1>
+            <p>PARIS</p>
+            <p>FRANCE</p>
+        </section>
+    );
+}
+
+export default Footer;
